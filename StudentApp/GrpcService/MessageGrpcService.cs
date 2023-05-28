4c2c2ae8e0def6070b20afc9f8a521a6c9ee4af5
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-05-29 00:22:42 +0200

    MsgGrpServiceGetAllStudents

--- StudentApp/GrpcService/MessageGrpcService.cs
+++ StudentApp/GrpcService/MessageGrpcService.cs
@@ -1,15 +1,45 @@
 ﻿
+using Azure;
 using Grpc.Core;
+using StudentApp.Models;
+using StudentApp.Services;
 
 namespace StudentApp.GrpcService;
 
 public class MessageGrpcService : GrpcMessage.Message.MessageBase
 {
-
+    private readonly IService _service;
     private readonly ILogger<MessageGrpcService> _logger;
-    public MessageGrpcService(ILogger<MessageGrpcService> logger)
+    public MessageGrpcService(IService service, ILogger<MessageGrpcService> logger)
     {
+        _service = service;
         _logger = logger;
+    }
+
+    public override async Task GetAllStudents(GrpcMessage.GetAllStudentsRequest request, IServerStreamWriter<GrpcMessage.GetAllStudentsResponse> responseStream, ServerCallContext context)
+    {
+        var students = new List<StudentResponse>();
+        var result = await _service.Get();
+        if (result is null)
+        {
+            _logger.LogDebug("Students data not retrieved from the service.");
+            await responseStream.WriteAsync(new GrpcMessage.GetAllStudentsResponse());
+        }
+        else
+        {
+            for (var i = 0; i < result.Count; i++)
+            {
+                await responseStream.WriteAsync(new GrpcMessage.GetAllStudentsResponse { 
+                    Id = result[i].StudentId,
+                    FirstName = result[i].FirstName,
+                    SecondName = result[i].LastName,
+                    LastName = result[i].LastName,
+                    UserName = result[i].UserName,
+                    School  = result[i].School
+                });
+            }
+
+        }
     }
 
     public override async Task GetAllMessages(GrpcMessage.GetAllMessagesRequest request, IServerStreamWriter<GrpcMessage.GetAllMessagesResponse> responseStream, ServerCallContext context)
